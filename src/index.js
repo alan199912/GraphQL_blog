e7bb85b158d2d2e7ddd55e9a477af157f2c3d9da
Alan199912 <alan199912@gmail.com>
2022-08-28 12:40:58 -0300

    feat: implement cors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./graphql/schema");
 const { connectDB } = require("./db");
 const { authenticate } = require("./middlewares/auth");
+const cors = require("cors");
 
 connectDB();
 const app = express();
-
+app.use(cors({ origin: "http://localhost:3000" }));
 app.listen(5000);
 
-app.use(authenticate);
+// FIXME: we dont use this anymore
+// app.use(authenticate);
 
 app.get("/", (req, res) => {
   res.send("Welcome to blog GraphQL & NodeJS");
